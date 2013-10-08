eeb57e637589ff5d8205cfd597638ae3978cc6ee
michaelt <what_is_it_to_do_anything@yahoo.com>
2013-10-08 10:07:50 -0400

    signature, no case

--- listing2b.hs
+++ listing2b.hs
@@ -4,7 +4,7 @@ import System.Environment
 main :: IO ()
 main = getArgs >>= actOnArgs
 
-
+actOnArgs :: [String] -> IO ()
 actOnArgs []            = putStrLn "No arguments!"
 actOnArgs [arg]         = putStrLn (arg ++ " is nice, but we need *two* arguments")
 actOnArgs (arg1:arg2:_) = putStrLn ("Hello, " ++ arg1 ++ ", your second argument is " ++ arg2)
