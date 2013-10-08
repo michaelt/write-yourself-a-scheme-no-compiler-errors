d4e513ee967c9a2574e6672df11da9ab82f7439c
michaelt <what_is_it_to_do_anything@yahoo.com>
2013-10-08 10:02:37 -0400

    without case

---
+++ listing2b.hs
@@ -0,0 +1,10 @@
+module Main where
+import System.Environment
+
+main :: IO ()
+main = getArgs >>= actOnArgs
+
+
+actOnArgs []            = putStrLn "No arguments!"
+actOnArgs [arg]         = putStrLn (arg ++ " is nice, but we need *two* arguments")
+actOnArgs (arg1:arg2:_) = putStrLn ("Hello, " ++ arg1 ++ ", your second argument is " ++ arg2)
