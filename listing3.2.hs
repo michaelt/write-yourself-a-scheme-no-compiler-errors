e3af3e3fe54cf21afd07e2e68f46f2ec78593752
michaelt <what_is_it_to_do_anything@yahoo.com>
2013-10-08 09:46:10 -0400

    maybe they compile

---
+++ listing3.2.hs
@@ -0,0 +1,17 @@
+module Main where
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+main :: IO ()
+main = do args <- getArgs
+          putStrLn (readExpr (args !! 0))
+
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=>?@^_~#"
+
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
